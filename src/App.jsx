8400530b487c55ee6f2b9a7f45fc7d03a0ca25ad
Josell Vibar <vibarjosell@gmail.com>
2025-02-05 18:10:47 +0800

    fix importing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Header from './components/header'
-import Main from './components/main'
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
 import './App.css'
 
 export default function App() {
